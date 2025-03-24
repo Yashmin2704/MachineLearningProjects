ea5aa1cdbda0e0fe925ade8bae05cef5ad38679d
Yashmin2704 <y.mohapatra27@gmail.com>
2025-03-24 14:02:35 +0530

    Add files via upload

---
+++ Medical_No_show/ModelClassification.py
@@ -0,0 +1,265 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+from sklearn.pipeline import Pipeline,FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.metrics import mean_squared_error
+
+from mypipes import *
+
+import seaborn as sns
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+from sklearn.metrics import roc_auc_score, roc_curve
+import matplotlib.pyplot as plt
+
+
+
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_curve, roc_auc_score
+import matplotlib.pyplot as plt
+
+def report(results, n_top=3):
+    for i in range(1, n_top + 1):
+        candidates = np.flatnonzero(results['rank_test_score'] == i)
+        for candidate in candidates:
+            print("Model with rank: {0}".format(i))
+            print("Mean validation score: {0:.6f} (std: {1:.6f})".format(
+                  results['mean_test_score'][candidate],
+                  results['std_test_score'][candidate]))
+            print("Parameters: {0}".format(results['params'][candidate]))
+            print("")
+
+def plot_roc_curve(true_y, y_prob):
+    """
+    plots the roc curve based of the probabilities
+    """
+
+    fpr, tpr, thresholds = roc_curve(true_y, y_prob)
+    plt.plot(fpr, tpr)
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    
+    score = roc_auc_score(true_y, y_prob)
+    print(f"ROC AUC: {score:.4f}")
+
+
+
+#Logistic Regression
+def LogisticR(x_train,y_train,x_test,y_test):
+    params={'class_weight':['balanced',None],'penalty':['l1','l2'],'C':[.0001,.0005,.001,.005,.01,.05,.1,1,2,5]}
+    model=LogisticRegression(fit_intercept=True)
+    '''grid_search=GridSearchCV(model, param_grid=params,cv=10,
+                         scoring="roc_auc",
+                         n_jobs=-1,
+                         verbose=20)
+    grid_search.fit(x_train,y_train)
+    logr=grid_search.best_estimator_
+    print(report(grid_search.cv_results_,5))'''
+
+
+    logr=LogisticRegression(fit_intercept=True,
+                            **{'C': 0.05, 'class_weight': 'balanced', 'penalty': 'l2'},solver='liblinear')
+    # default solver lbfgs does not support l1 penalty for some versions of sklearn
+    # if you get an error like that , simply use solver='liblinear', it supports both l1 & l2 penalty
+
+
+    logr.fit(x_train,y_train)
+    
+
+    print(logr.intercept_)
+
+    a=list(zip(x_train.columns,logr.coef_[0]))
+    print(a)
+
+    print(logr.predict_proba(x_train))
+
+    
+
+    cutoffs=np.linspace(0.01,0.99,99)
+
+    
+
+    logr.predict_proba(x_train)
+
+
+    train_score=logr.predict_proba(x_train)[:,1]
+    real=y_train
+    print("train score")
+    print(train_score)
+    print("real")
+    print(real)
+     # In  order to find the probability of which column is for outcome 1 and which for outcome 0
+
+
+    (train_score>0.2).astype(int)
+
+    KS_all=[]
+    Acc_all=[]
+    Recall_all=[]
+    Precission_all=[]
+    F1_all=[]
+    Sp_all=[]
+
+    for cutoff in cutoffs:
+
+        predicted=(train_score>cutoff).astype(int)
+        
+        if isinstance(real, pd.DataFrame):
+            real = real.iloc[:, 0]
+
+        TP=((predicted==1) & (real==1)).sum()
+        TN=((predicted==0) & (real==0)).sum()
+        FP=((predicted==1) & (real==0)).sum()
+        FN=((predicted==0) & (real==1)).sum()
+
+        P=TP+FN
+        N=TN+FP
+
+
+        KS=(TP/P)-(FP/N)
+        Acc=(TP+TN)/(P+N)
+        Recall=TP/(TP+FN)
+        pre=TP/(TP+FP)
+        F1=(2*Recall*pre)/(Recall+pre)
+        sp=TN/(FP+TN) #specification
+
+        KS_all.append(KS)
+        Acc_all.append(Acc)
+        Recall_all.append(Recall)
+        Precission_all.append(pre)
+        F1_all.append(F1)
+        Sp_all.append(sp)
+
+
+
+
+    list(zip(cutoffs,KS_all))
+
+    mycutoff=cutoffs[KS_all==max(KS_all)]
+    print(pd.DataFrame({'Cut-Off': cutoffs,
+              'F1':F1_all, 
+              'Precision':Precission_all,
+              'Recall':Recall_all,
+             'Acc': Acc_all,
+             'KS':KS_all}))
+    print("beta0:",logr.intercept_)
+    b=list(zip(x_train.columns,logr.coef_[0]))
+    print(b)
+    print(logr.predict_proba(x_test))
+    test_score=logr.predict_proba(x_test)[:,1]
+    print(test_score)
+    test_classes=(test_score>mycutoff).astype(int)
+    print(roc_curve(y_test, test_score))
+    print(plot_roc_curve(y_test, test_score))
+    return(test_classes)
+
+
+#decision tree
+def decision(x_train,y_train,x_test,y_test):
+
+    params={'class_weight':[None,'balanced'], 
+            'max_depth':[None,5,10,15,20,30,50,70],
+            'min_samples_leaf':[1,2,5,10,15,20], 
+            'min_samples_split':[2,5,10,15,20]
+           }
+
+    clf=DecisionTreeClassifier()
+
+    '''random_search=RandomizedSearchCV(clf,cv=5,param_distributions=params,scoring='f1',n_iter=240,n_jobs=-1,verbose=20)
+    dtree_fit=random_search.fit(x_train,y_train)
+    print(report(random_search.cv_results_,5))'''
+
+    dtree=DecisionTreeClassifier(**{'min_samples_split': 2, 
+                                    'min_samples_leaf': 15, 
+                                    'max_depth': 5, 
+                                    'class_weight': 'balanced'})
+
+    dtree_fit =dtree.fit(x_train,y_train)
+
+    test_score=dtree_fit.predict_proba(x_test)[:,1]
+    
+
+    y_pred = dtree_fit.predict(x_test)
+        
+    print(classification_report(y_test, y_pred, target_names=['0', '1']))
+
+   # print(pd.crosstab(y_test, y_pred))
+    
+    print(roc_curve(y_test, test_score))
+    print(plot_roc_curve(y_test, test_score))
+
+    #dtree_fit.predict(pd.DataFrame(x_test.iloc[[5]]))
+    return(test_score)
+    
+    
+    
+    
+def random(x_train,y_train,x_test,y_test):
+    clf = RandomForestClassifier()
+
+    param_dist = {"n_estimators":[100,200,300,500,700,1000],
+              "max_features": [5,10,20,25,30,35],
+              "bootstrap": [True, False],
+              'class_weight':[None,'balanced'], 
+              'max_depth':[None,5,10,15,20,30,50,70],
+              'min_samples_leaf':[1,2,5,10,15,20], 
+              'min_samples_split':[2,5,10,15,20]
+                  }
+    
+    random_search = RandomizedSearchCV(clf, 
+                                       param_distributions=param_dist,
+                                       n_iter=5,
+                                       scoring='f1',
+                                       cv=5,
+                                       n_jobs=-1,
+                                       verbose=20)
+    '''rf=random_search.fit(x_train, y_train)
+
+    print(random_search.best_estimator_)
+
+    RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini',
+            max_depth=50, max_features=10, max_leaf_nodes=None,
+            min_impurity_decrease=1e-07, min_samples_leaf=10,
+            min_samples_split=20, min_weight_fraction_leaf=0.0,
+            n_estimators=300, n_jobs=1, oob_score=False, random_state=None,
+            verbose=0, warm_start=False)
+
+
+    report(random_search.cv_results_,3)'''
+    
+    # select the best values from results above, they will vary slightly with each run
+    rf = RandomForestClassifier(**{'n_estimators': 500, 
+                               'min_samples_split': 20, 
+                               'min_samples_leaf': 10, 
+                               'max_features': 10, 
+                               'max_depth': 50, 
+                               'class_weight': 'balanced', 
+                               'bootstrap': True})
+    rf.fit(x_train,y_train)
+
+    y_pred_rf = rf.predict(x_test)
+    test_score=rf.predict_proba(x_test)[:,1]
+
+    print(classification_report(y_test, y_pred_rf, target_names=['0', '1']))
+
+    #print(pd.crosstab(y_test, y_pred_rf))
+    print(roc_curve(y_test, test_score))
+    print(plot_roc_curve(y_test, test_score))
+    '''feat_imp_df=pd.DataFrame({'features':x_train.columns,
+                          'importance':rf.feature_importances_})
+
+    feat_imp_df=feat_imp_df.sort_values('importance',ascending=False)
+    feat_imp_df['cum_imp']=np.cumsum(feat_imp_df['importance'])
+    x=feat_imp_df.reset_index()
+    print(x_train[x[:500]['features']])'''
+    return(y_pred_rf)
---
+++ Medical_No_show/mypipes.py
@@ -0,0 +1,317 @@
+import pandas as pd
+import numpy as np
+import re
+
+from sklearn.pipeline import Pipeline,FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class VarSelector(BaseEstimator, TransformerMixin):
+
+    def __init__(self,feature_names):
+
+        self.feature_names=feature_names
+
+
+    def fit(self,x,y=None):
+
+        return self
+
+    def transform(self,X):
+
+        return X[self.feature_names]
+
+    def get_feature_names(self):
+
+        return self.feature_names
+    
+    def get_feature_names_out(self, feature_names_out):
+
+        return self.feature_names
+
+
+class custom_fico(BaseEstimator,TransformerMixin):
+
+    def __init__(self):
+
+        self.feature_names=['fico']
+
+    def fit(self,x,y=None):
+
+        return self
+
+    def transform(self,X):
+
+        k=X['FICO.Range'].str.split('-',expand=True).astype(float)
+        fico=0.5*(k[0]+k[1])
+        return pd.DataFrame({'fico':fico})
+
+    def get_feature_names(self):
+
+        return self.feature_names
+    
+    def get_feature_names_out(self, feature_names_out):
+
+        return self.feature_names
+
+
+class custom_age_band(BaseEstimator,TransformerMixin):
+
+    def __init__(self):
+
+        self.feature_names=['age_band']
+
+    def fit(self,x,y=None):
+
+        return self
+
+    def transform(self,X):
+
+        k=X['age_band'].str.split('-',expand=True)
+        k[0]=pd.to_numeric(k[0],errors='coerce')
+        k[1]=pd.to_numeric(k[1],errors='coerce')
+        age_band=0.5*(k[0]+k[1])
+        age_band=np.where(X['age_band'].str[:2]=='71',71,age_band)
+        return pd.DataFrame({'age_band':age_band})
+
+    def get_feature_names(self):
+
+        return self.feature_names
+    
+    def get_feature_names_out(self, feature_names_out):
+
+        return self.feature_names
+
+
+class custom_family_income(BaseEstimator,TransformerMixin):
+
+    def __init__(self):
+
+        self.feature_names=['fi']
+
+    def fit(self,x,y=None):
+
+        return self
+
+    def transform(self,X):
+
+        k=X['family_income'].str.replace(',','')
+        k=k.str.replace('<','')
+        k=k.str.replace('>=','')
+        k=k.str.strip()
+        k=k.str.replace('  ',' ')
+        a=k.str.split(' ',expand=True)
+
+        a[0]=pd.to_numeric(a[0],errors='coerce')
+        a[1]=pd.to_numeric(a[1],errors='coerce')
+        fi=0.5*(a[0]+a[1])
+
+        fi=np.where(k=='35000',35000,fi)
+        fi=np.where(k=='4000',4000,fi)
+
+        return pd.DataFrame({'fi':fi})
+
+    def get_feature_names(self):
+
+        return self.feature_names
+    
+    def get_feature_names_out(self, feature_names_out):
+
+        return self.feature_names
+
+
+        
+
+class string_clean(BaseEstimator, TransformerMixin):
+
+    def __init__(self,replace_it='',replace_with=''):
+
+        self.replace_it=replace_it
+        self.replace_with=replace_with
+        self.feature_names=[]
+
+    def fit(self,x,y=None):
+
+        self.feature_names=x.columns
+        return self
+
+    def transform(self,X):
+
+        for col in X.columns:
+            X[col]=X[col].str.replace(self.replace_it,self.replace_with)
+        return X
+    def get_feature_names(self):
+
+        return self.feature_names
+    
+    def get_feature_names_out(self, feature_names_out):
+
+        return self.feature_names
+
+
+
+
+class convert_to_numeric(BaseEstimator, TransformerMixin):
+
+    def __init__(self):
+
+        self.feature_names=[]
+
+    def fit(self,x,y=None):
+        self.feature_names=x.columns
+        return self
+
+    def transform(self,X):
+        for col in X.columns:
+            X[col]=pd.to_numeric(X[col],errors='coerce')
+        return X
+    def get_feature_names(self):
+        return self.feature_names
+     
+    def get_feature_names_out(self, feature_names_out):
+        return self.feature_names
+
+
+class get_dummies_Pipe(BaseEstimator, TransformerMixin):
+
+    def __init__(self,freq_cutoff=0):
+
+        self.freq_cutoff=freq_cutoff
+        self.var_cat_dict={}
+        self.feature_names=[]
+
+    def fit(self,x,y=None):
+
+        data_cols=x.columns
+
+        for col in data_cols:
+
+            k=x[col].value_counts()
+
+            if (k<=self.freq_cutoff).sum()==0:
+                cats=k.index[:-1]
+
+            else:
+                cats=k.index[k>self.freq_cutoff]
+
+            self.var_cat_dict[col]=cats
+
+        for col in self.var_cat_dict.keys():
+            for cat in self.var_cat_dict[col]:
+                self.feature_names.append(col+'_'+str(cat))
+        return self
+
+    def transform(self,x,y=None):
+        dummy_data=x.copy()
+
+        for col in self.var_cat_dict.keys():
+            for cat in self.var_cat_dict[col]:
+                name=col+'_'+str(cat)
+                dummy_data[name]=(dummy_data[col]==cat).astype(int)
+
+            del dummy_data[col]
+        return dummy_data
+
+    def get_feature_names(self):
+
+        return self.feature_names
+    
+    def get_feature_names_out(self, feature_names_out):
+
+        return self.feature_names
+
+class Date_data(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.feature_names = ['day_sin', 'day_cos', 'month_sin', 'month_cos', 'weekday_sin', 'weekday_cos']
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        df = X.copy()
+        df['ListDate'] = pd.to_datetime(df['ListDate'])
+
+        # Extract date components
+        df['day'] = df['ListDate'].dt.day
+        df['month'] = df['ListDate'].dt.month
+        df['weekday'] = df['ListDate'].dt.dayofweek
+
+        # Normalize components
+        df['day_normalized'] = df['day'] / 31.0
+        df['month_normalized'] = df['month'] / 12.0
+        df['weekday_normalized'] = df['weekday'] / 7.0
+
+        # Apply sine and cosine transformations
+        df['day_sin'] = np.sin(2 * np.pi * df['day_normalized'])
+        df['day_cos'] = np.cos(2 * np.pi * df['day_normalized'])
+        df['month_sin'] = np.sin(2 * np.pi * df['month_normalized'])
+        df['month_cos'] = np.cos(2 * np.pi * df['month_normalized'])
+        df['weekday_sin'] = np.sin(2 * np.pi * df['weekday_normalized'])
+        df['weekday_cos'] = np.cos(2 * np.pi * df['weekday_normalized'])
+
+        df_transformed = df[self.feature_names]
+        return df_transformed
+
+    def get_feature_names_out(self, input_features=None):
+        return self.feature_names
+
+    
+class DateDifferenceExtractor(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.feature_names = ['date_difference']
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        df = X.copy()
+        # Convert the 'ScheduledDay' and 'AppointmentDay' columns to datetime type
+        df['ScheduledDay'] = pd.to_datetime(df['ScheduledDay'], errors='coerce')
+        df['AppointmentDay'] = pd.to_datetime(df['AppointmentDay'], errors='coerce')
+
+        # Calculate the difference in days between the two dates
+        df['date_difference'] = (df['AppointmentDay'] - df['ScheduledDay']).dt.days
+
+        # Select the 'date_difference' column to return
+        df_transformed = df[['date_difference']]
+        return df_transformed
+
+    def get_feature_names_out(self, input_features=None):
+        return self.feature_names
+    
+class DataFrameImputer(BaseEstimator,TransformerMixin):
+
+    def __init__(self):
+
+        self.impute_dict={}
+        self.feature_names=[]
+
+    def fit(self, X, y=None):
+
+        self.feature_names=X.columns
+
+        for col in X.columns:
+            if X[col].dtype=='O':
+                self.impute_dict[col]='missing'
+            else:
+                self.impute_dict[col]=X[col].median()
+        return self
+
+    def transform(self, X, y=None):
+        return X.fillna(self.impute_dict)
+
+    def get_feature_names(self):
+
+        return self.feature_names
+    
+    def get_feature_names_out(self, feature_names_out):
+
+        return self.feature_names
+
+class pdPipeline(Pipeline):
+
+    def get_feature_names(self):
+
+        last_step = self.steps[-1][-1]
+
+        return last_step.get_feature_names()
